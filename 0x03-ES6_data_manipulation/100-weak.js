3b3f62fc8b9ef2cd05e704a4c6d3def739d5dc39
nuru <0xnuru@gmail.com>
2024-06-04 23:04:10 +0100

    add 11-...

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let c = weakMap.get(endpoint) || 0;
+
+  c += 1;
+
+  weakMap.set(endpoint, c);
+
+  if (c >= 5) throw Error("Endpoint load is high");
+
+  return c;
+}
