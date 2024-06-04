6ac338838bf90e085d85249e63393aa47fd43973
nuru <0xnuru@gmail.com>
2024-06-04 22:19:45 +0100

    add mod for 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,10 @@
 /* eslint-disable quotes */
 function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
-  }
-
+  if (position >= length) throw Error("Position outside range");
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
-  view[position] = value;
-
-  return buffer;
+  view.set([value], position);
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
