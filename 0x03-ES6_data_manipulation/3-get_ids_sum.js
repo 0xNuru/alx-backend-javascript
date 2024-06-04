875f824e5f90d515d5985ba5798d73e0ad7c3adc
nuru <0xnuru@gmail.com>
2024-06-04 16:41:56 +0100

    add 3-get_ids_sum.js fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,3 @@
-export default function getStudentIdsSum(studentsObj) {
-  let initial_value = 0;
-  return studentsObj.reduce((sum, student) => sum + student.id, initial_value);
+export default function getStudentIdsSum(students) {
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
 }
